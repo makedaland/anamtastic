9fe3c3f68dfa75b37082384799471463cef3561a
wvxvw <olegsivokon@gmail.com>
2017-01-31 19:54:52 +0200

    Added stroke suspect and time of onset

--- app/src/main/java/tld/anamtastic/www/anamtastic/TimeOfOnsetActivity.java
+++ app/src/main/java/tld/anamtastic/www/anamtastic/TimeOfOnsetActivity.java
@@ -1,20 +1,16 @@
 package tld.anamtastic.www.anamtastic;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class SkinDiagnosticActivity extends AppCompatActivity {
+public class TimeOfOnsetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_skin_diagnostic);
+        setContentView(R.layout.activity_time_of_onset);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-
 }
